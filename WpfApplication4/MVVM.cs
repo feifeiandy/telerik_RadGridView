0531aa54547020f78bd378d2ebbf99376a654153
feifeiandy <feifeiandy@outlook.com>
2016-06-03 15:03:16 +0800

    r

---
+++ WpfApplication4/MVVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication4
+{
+    public class MVVM
+    {
+        public ObservableCollection<Student> MyProperty { get; set; }
+
+        public MVVM()
+        {
+            MyProperty = new ObservableCollection<Student>() 
+            {
+                new Student { ID = 1, Name = "text1", Date = DateTime.Now },
+                new Student { ID = 1, Name = "text2", Date = DateTime.Now },
+
+                new Student { ID = 2, Name = "text5", Date = DateTime.Now }, 
+                new Student { ID = 2, Name = "text6", Date = DateTime.Now }, 
+                new Student { ID = 2, Name = "text7", Date = DateTime.Now } 
+            };
+        }
+    }
+}
---
+++ WpfApplication4/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls.GridView;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.tk.BeginInsert();
+        }
+
+        private void tk_AddingNewDataItem(object sender, Telerik.Windows.Controls.GridView.GridViewAddingNewEventArgs e)
+        {
+            e.NewObject = new Student();
+        }
+
+        private void tk_RowEditEnded(object sender, Telerik.Windows.Controls.GridViewRowEditEndedEventArgs e)
+        {
+            if (e.EditAction == GridViewEditAction.Cancel)
+            {
+                return;
+            }
+            if (e.EditOperationType == GridViewEditOperationType.Insert)
+            {
+                Student stu1 = e.NewData as Student;
+                MessageBox.Show(stu1.ID.ToString());
+            }
+            if (e.EditOperationType == GridViewEditOperationType.Edit)
+            {
+                Student stu1 = e.NewData as Student;
+                MessageBox.Show(stu1.Name);
+            }
+
+        }
+    }
+}
